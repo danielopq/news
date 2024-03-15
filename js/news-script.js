a17552498bc387c799d77003f053211053440bcb
Daniel Martínez Duque <danielopq@gmail.com>
2024-03-15 02:29:31 +0000

    final version done reviewed

--- js/news-script.js
+++ js/news-script.js
@@ -1,1 +1,14 @@
-// JavaScript Document
+window.onload = start;
+
+function start(){
+	document.getElementById("menu-hamburger").addEventListener('click',openMobileMenu,false);
+	document.getElementById("closeIcon-img").addEventListener('click',closeMobileMenu,false);
+}
+
+function openMobileMenu(){
+	document.getElementById("menu-mobile").style.display = "block";
+}
+
+function closeMobileMenu(){
+	document.getElementById("menu-mobile").style.display = "none";
+}
