2a95ab9074e6a37605b44df0d8012aebe6910c8a
danielopq <danielopq@gmail.com>
2024-08-04 15:48:28 +0100

    news-script.js, index.html - JS cleanning code

--- js/news-script.js
+++ js/news-script.js
@@ -1,14 +1,19 @@
-window.onload = start;
 
-function start(){
+(()=>{
 	document.getElementById("menu-hamburger").addEventListener('click',openMobileMenu,false);
 	document.getElementById("closeIcon-img").addEventListener('click',closeMobileMenu,false);
-}
+})()
 
+/**
+ * Displays the main menu (mobile version).
+ */
 function openMobileMenu(){
 	document.getElementById("menu-mobile").style.display = "block";
 }
 
+/**
+ * Hide the main menu (mobile version).
+ */
 function closeMobileMenu(){
 	document.getElementById("menu-mobile").style.display = "none";
 }
